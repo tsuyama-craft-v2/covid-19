4e02258a1f604190438a0d4985587714915ff426
tomosue <nikoniko04211970@gmail.com>
2022-09-13 11:56:22 +0900

    Update vue.config.js
    buildファイルを変更

---
+++ docs/precache-manifest.4e257bddcd6d7f95ba794e11e03b3aa3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a860caeae2648b52830",
+    "url": "css/app.8f8cf039.css"
+  },
+  {
+    "revision": "b5f6b5522bd428427beb38672999c5b3",
+    "url": "data/330001okayamacovid19medicalsystem.csv"
+  },
+  {
+    "revision": "74d07f2ba525240672bcad6d46c48cdd",
+    "url": "data/AED_location.csv"
+  },
+  {
+    "revision": "10a8e2a41c46e793c8f5494ebe7b4c91",
+    "url": "data/Children_location.csv"
+  },
+  {
+    "revision": "9a7bedff9487b1a6ec37ceca1eac6fdc",
+    "url": "data/Government_location.csv"
+  },
+  {
+    "revision": "5782be75c61e12334828ab94fc43eb3e",
+    "url": "data/JuniorHighSchool_location.csv"
+  },
+  {
+    "revision": "bf29f3bc8e3a58dfa5c7cd8242ae25da",
+    "url": "data/JuniorSchool_location.csv"
+  },
+  {
+    "revision": "1da10d6f4015bc16ec07c08fe9b44af4",
+    "url": "data/Kindergarten_location.csv"
+  },
+  {
+    "revision": "778d2fe6a42d8b34cac76d048fd1ac3d",
+    "url": "data/Library_location.csv"
+  },
+  {
+    "revision": "66e4826c866c7b8b49b3ad502a1025dc",
+    "url": "data/Nursery_location.csv"
+  },
+  {
+    "revision": "880ad846379e1501f35e97d917fc9032",
+    "url": "data/Okayama_iryou.csv"
+  },
+  {
+    "revision": "29c8baf1ede143545783d50e403fc9f3",
+    "url": "data/Okayama_shika.csv"
+  },
+  {
+    "revision": "167371de7733f2f9c3c296b38d450de8",
+    "url": "data/Okayama_yakkyoku.csv"
+  },
+  {
+    "revision": "ae583806aaf39cb7406f7a7772405cb2",
+    "url": "data/Park_location.csv"
+  },
+  {
+    "revision": "30993fcaea8663ddd565cee014b80002",
+    "url": "data/Rally_location.csv"
+  },
+  {
+    "revision": "9f1891f15dd5660a62464428300909ce",
+    "url": "data/Sightseeing_location.csv"
+  },
+  {
+    "revision": "265b9be4776b5bf24f195a19acfe37d1",
+    "url": "data/Sports_location.csv"
+  },
+  {
+    "revision": "1b661cd81a9b746a18937d356b185beb",
+    "url": "data/Welfare_location.csv"
+  },
+  {
+    "revision": "767ebbd9191728be65db39a43dba7ab0",
+    "url": "data/kanjahasseijoukyou.csv"
+  },
+  {
+    "revision": "cd1a15ca0aeefa9fe97d024528676b5c",
+    "url": "data/kansenshashousaijouhou.csv"
+  },
+  {
+    "revision": "aac082cbe02a397e28a01e6845565ff6",
+    "url": "data/kansenshasuu0420.csv"
+  },
+  {
+    "revision": "541b9557d7c6670e030f52ccc4fd305a",
+    "url": "data/pcr.csv"
+  },
+  {
+    "revision": "127965e6fc5200fd35e6cb99ba282d39",
+    "url": "img/yajirushi.127965e6.png"
+  },
+  {
+    "revision": "0fe1037abfe22cc954219ca18954ed9c",
+    "url": "index.html"
+  },
+  {
+    "revision": "5a860caeae2648b52830",
+    "url": "js/app.3ec656fe.js"
+  },
+  {
+    "revision": "2463193dd32e49003b0f",
+    "url": "js/chunk-vendors.93c6e3a2.js"
+  },
+  {
+    "revision": "d3b60e3c370fea96745b8fe1a122ed86",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
